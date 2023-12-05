7b4b1b4b54941e19ccd1699efda25f75f2616484
miguel ayala <miguel.ayala.au@gmail.com>
2023-12-05 14:27:51 +1000

    update function

--- r_setup.R
+++ r_setup.R
@@ -1,4 +1,3 @@
-
 r_setup <- function(){
   
   rm(list = ls()[ls()!="path"])   # clear workspace - with the exception of the working directory
@@ -14,19 +13,19 @@ r_setup <- function(){
                         "leaps","tseries","urca","writexl",
                         "reshape2","cowplot","ggplotify",
                         "scales","readabs","caTools","zoo",
-                        'rmarkdown','rstudioapi','extrafont')
+                        'rmarkdown','rstudioapi','extrafont',"readrba", "readabs", "fpp3","sf")
   new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
   if(length(new.packages)) {install.packages(new.packages)}
   invisible(lapply(list.of.packages, require, character.only = TRUE))
   
   # if(!"Haver" %in% installed.packages()){options(install.packages.check.source = FALSE)
   #   install.packages("Haver", repos="http://www.haver.com/r/") }
-  
+  # 
   # require("Haver")
+  # 
+  # if(is.null(haver.path())){haver.path("auto")}
   
-  #if(is.null(haver.path())){haver.path("auto")}
+  #if(!(file.exists('C:/Windows/Fonts/segoeui.ttf'))){font_import()}
   
-  if(!(file.exists('C:/Windows/Fonts/segoeui.ttf'))){font_import()}
-  
-  windowsFonts(`Segoe UI` = windowsFont('Segoe UI'))
+  #windowsFonts(`Segoe UI` = windowsFont('Segoe UI'))
 }
