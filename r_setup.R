85f82ca776b8c07c85075bac6e0062051a3d77e5
miguel ayala <miguel.ayala.au@gmail.com>
2023-12-06 20:18:03 +1000

    add new package

--- r_setup.R
+++ r_setup.R
@@ -14,7 +14,7 @@ r_setup <- function(){
                         "reshape2","cowplot","ggplotify",
                         "scales","readabs","caTools","zoo",
                         'rmarkdown','rstudioapi','extrafont',"readrba", "readabs", "fpp3","sf",
-                        "shinyjs", "shinydashboard")
+                        "shinyjs", "shinydashboard","shinyWidgets")
   new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
   if(length(new.packages)) {install.packages(new.packages)}
   invisible(lapply(list.of.packages, require, character.only = TRUE))
